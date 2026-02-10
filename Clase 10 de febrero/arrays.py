38f7f3c59ac8e41d3d3f068cfb61d994c42ce73c
María Victoria <maria.arteagare@campusucc.edu.co>
2026-02-10 18:59:08 -0500

    Primera clase

---
+++ Clase 10 de febrero/arrays.py
@@ -0,0 +1,19 @@
+# First script. 
+
+# Existen arrays 
+
+# Unidimencional.
+my_list1: list = [58,56,23,45,15]
+
+# Bidimencional. 
+my_list2: list = [[58,56,23,45,15], 
+                [15,45,56,25,23],
+                [25,85,46,51,46]]
+
+# pop = elimina por el indice / por default elimina el último elemento.
+# remove = elimina por valor del elemento.
+# del = no retorna. Elimina el elemento en una posición.
+# .index = Busca por el valor del elemento.
+
+# sort = elimina de la a - z
+# sort(reverse = True) ordena de la z - a
---
+++ Clase 10 de febrero/taller1.py
@@ -0,0 +1,23 @@
+# First exercise 
+
+# Create a list with 5 elements
+colors: list = ['Amarillo', 'Azul', 'Rojo', 'Verde', 'Morado']
+print(colors[1]) # 
+colors.insert(3, "Estructura de datos")
+
+for index, color in enumerate(colors):
+    if color == "Estructura de datos":
+        print(index)
+
+
+# Create a list 3x3.
+ages: list = [[15,25,33],
+            [45,28,36],
+            [36,55,87]]
+
+print(ages[1][1])
+del ages[2][2]
+
+for index, age in enumerate(ages):
+    if age == 55:
+        print(index)
